1d372ca3a81a6e189a55a5d4879790a6c580a0a8
rerro <101159509+rorre0@users.noreply.github.com>
2023-03-30 19:20:51 +0900

    home, footer 수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import { useState } from 'react';
-
 const Footer = () => {
-  let [familySiteName, b] = useState(
+  let [familySiteName, b] = useState([
     '야놀자',
     '야놀자 비즈니스',
     '이지테크노시스',
     '젠룸스',
     '산하정보기술',
-    '데일리호텔'
-  );
+    '데일리호텔',
+  ]);
 
   return (
     <footer className="footer">
@@ -23,11 +23,11 @@ const Footer = () => {
         </address>
         <div className="footer__addInfo">
           <div className="footer__familySite">
-            <div className="footer__show">
-              <h6 className="footer__familySiteName"></h6>
+            <div className="footer__siteListBtn">
+              <h6 className="footer__familySiteTitle">Famliy site</h6>
               <span>X</span>
             </div>
-            <div className="footer__hide">
+            <div className="footer__siteListCont">
               <p>
                 <a href="#">{familySiteName[0]}</a>
               </p>
@@ -48,9 +48,34 @@ const Footer = () => {
               </p>
             </div>
           </div>
-          <div className="footer__followUs"></div>
+          <div className="footer__followUs">
+            <div className="footer__followUsTitle">Follow Us</div>
+            <span className="footer__snsImg">
+              <img src="" alt="Linked in" />
+            </span>
+            <span className="footer__snsImg">
+              <img src="" alt="Facebook" />
+            </span>
+            <span className="footer__snsImg">
+              <img src="" alt="Instargram" />
+            </span>
+            <span className="footer__snsImg">
+              <img src="" alt="naver post" />
+            </span>
+            <span className="footer__snsImg">
+              <img src="" alt="youtub" />
+            </span>
+            <span className="footer__snsImg">
+              <img src="" alt="소비자중심 공정거래위원회" />
+            </span>
+            <span className="footer__designAward">
+              <img src="" alt="Design Award 2022" />
+            </span>
+          </div>
         </div>
       </section>
     </footer>
   );
 };
+
+export default Footer;
