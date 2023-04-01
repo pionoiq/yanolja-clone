9bde268d6c336cf7b95c1f8ab4d60ffe77e71c07
rerro <101159509+rorre0@users.noreply.github.com>
2023-04-01 22:19:46 +0900

    scss 작업

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import './Footer.scss';
 import { useState } from 'react';
+// import { Logo } from '../../svg/footer_img/logo_b2.svg';
+
 const Footer = () => {
   let [familySiteName, b] = useState([
     '야놀자',
@@ -10,22 +13,31 @@ const Footer = () => {
     '데일리호텔',
   ]);
 
+  const [isOpen, setList] = useState(false);
+
   return (
     <footer className="footer">
       <section className="footer__section">
         <div className="footer__logo">
-          <img src="" alt="yanolja_logo" />
+          <img src="{Logo}" alt="yanolja_logo" />
         </div>
         <address className="footer__address">
-          <p className="footer__adrTop"></p>
-          <p className="footer__adrMid"></p>
-          <p className="footer__adrBtm"></p>
+          <div className="footer__adrTodiv">Y-Siren (윤리경영/제보)</div>
+          <div className="footer__adrMid">
+            <p className="footer__tel">TEL1644-1346</p>
+            <p className="footer__fax">FAX 02-558-4336</p>
+            <p className="footer__email">contact@yanolja.com</p>
+          </div>
+          <div className="footer__adrBtm">
+            <p className="footer__adr">서울특별시 강남구 테헤란로 108길 42 (대치동 1003)</p>
+            <p className="footer__codivyright">© Yanolja Co., Ltd.</p>
+          </div>
         </address>
         <div className="footer__addInfo">
           <div className="footer__familySite">
             <div className="footer__siteListBtn">
-              <h6 className="footer__familySiteTitle">Famliy site</h6>
-              <span>X</span>
+              {/* <div className="footer__familySiteTitle">Famliy site</div>
+              <span>X</span> */}
             </div>
             <div className="footer__siteListCont">
               <p>
