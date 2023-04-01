0a20a395791dadb56e18080cf10dc8dda63773e9
rerro <101159509+rorre0@users.noreply.github.com>
2023-04-02 02:43:58 +0900

    footer scss 수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,76 +13,90 @@ const Footer = () => {
     '데일리호텔',
   ]);
 
-  const [isOpen, setList] = useState(false);
+  const [isOpen, setMenu] = useState(false); // 메뉴의 초기값을 false로 설정
+
+  const toggleMenu = () => {
+    setMenu((isOpen) => !isOpen); // on,off 개념 boolean
+  };
 
   return (
     <footer className="footer">
       <section className="footer__section">
         <div className="footer__logo">
-          {/*<img src="{Logo}" alt="yanolja_logo" />*/}
+          <img src="{Logo}" alt="yanolja_logo" />
         </div>
         <address className="footer__address">
-          <div className="footer__adrTodiv">Y-Siren (윤리경영/제보)</div>
-          <div className="footer__adrMid">
-            <p className="footer__tel">TEL1644-1346</p>
-            <p className="footer__fax">FAX 02-558-4336</p>
-            <p className="footer__email">contact@yanolja.com</p>
-          </div>
-          <div className="footer__adrBtm">
-            <p className="footer__adr">서울특별시 강남구 테헤란로 108길 42 (대치동 1003)</p>
-            <p className="footer__codivyright">© Yanolja Co., Ltd.</p>
-          </div>
+          <ul>
+            <li className="footer__adrTodiv">Y-Siren (윤리경영/제보)</li>
+            <li className="footer__adrMid">
+              <p className="footer__tel">TEL1644-1346</p>
+              <p className="footer__fax">FAX 02-558-4336</p>
+              <p className="footer__email">contact@yanolja.com</p>
+            </li>
+            <li className="footer__adrBtm">
+              <p className="footer__adr">서울특별시 강남구 테헤란로 108길 42 (대치동 1003)</p>
+              <p className="footer__codivyright">© Yanolja Co., Ltd.</p>
+            </li>
+          </ul>
         </address>
         <div className="footer__addInfo">
           <div className="footer__familySite">
-            <div className="footer__siteListBtn">
-              {/* <div className="footer__familySiteTitle">Famliy site</div>
-              <span>X</span> */}
+            <div className="footer__familySiteTitle">
+              <span>Famliy site</span>
+              <span>×</span>
             </div>
+            <div className="footer__btnBox--covered"></div>
+            {/* <div className="footer__btnBox"></div> */}
             <div className="footer__siteListCont">
-              <p>
-                <a href="#">{familySiteName[0]}</a>
-              </p>
-              <p>
-                <a href="#">{familySiteName[1]}</a>
-              </p>
-              <p>
-                <a href="#">{familySiteName[2]}</a>
-              </p>
-              <p>
-                <a href="#">{familySiteName[3]}</a>
-              </p>
-              <p>
-                <a href="#">{familySiteName[4]}</a>
-              </p>
-              <p>
-                <a href="#">{familySiteName[5]}</a>
-              </p>
+              <ul>
+                <li>
+                  <a href="#">{familySiteName[0]}</a>
+                </li>
+                <li>
+                  <a href="#">{familySiteName[1]}</a>
+                </li>
+                <li>
+                  <a href="#">{familySiteName[2]}</a>
+                </li>
+                <li>
+                  <a href="#">{familySiteName[3]}</a>
+                </li>
+                <li>
+                  <a href="#">{familySiteName[4]}</a>
+                </li>
+                <li>
+                  <a href="#">{familySiteName[5]}</a>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="footer__followUs">
             <div className="footer__followUsTitle">Follow Us</div>
-            <span className="footer__snsImg">
-              <img src="" alt="Linked in" />
-            </span>
-            <span className="footer__snsImg">
-              <img src="" alt="Facebook" />
-            </span>
-            <span className="footer__snsImg">
-              <img src="" alt="Instargram" />
-            </span>
-            <span className="footer__snsImg">
-              <img src="" alt="naver post" />
-            </span>
-            <span className="footer__snsImg">
-              <img src="" alt="youtub" />
-            </span>
-            <span className="footer__snsImg">
-              <img src="" alt="소비자중심 공정거래위원회" />
-            </span>
-            <span className="footer__designAward">
-              <img src="" alt="Design Award 2022" />
-            </span>
+            <div className="footer__snsImg">
+              <ul>
+                <li>
+                  <img src="" alt="Linkedin" />
+                </li>
+                <li>
+                  <img src="" alt="Facebook" />
+                </li>
+                <li>
+                  <img src="" alt="Instargram" />
+                </li>
+                <li>
+                  <img src="" alt="naver" />
+                </li>
+                <li>
+                  <img src="" alt="youtub" />
+                </li>
+                <li>
+                  <img src="" alt="소비자중심공정거래위원회" />
+                </li>
+                <li>
+                  <img src="" alt="Design Award 2022" />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </section>
