692be6f301f90d6427eb3916498d2d9db9653df4
kimmmm126 <rlarkgml88@gmail.com>
2023-04-07 18:38:35 +0900

    상단 이동 버튼 스타일 수정 svg icon 추가 애니메이션 추가

--- src/assets/svg/Svg.js
+++ src/assets/svg/Svg.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-export const icon_top = (
+export const icon_arrow_top = (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.166 22.107">
     <defs>
-      <style>{`path{fill:none;stroke:#000;stroke-width:2px;}`}</style>
+      <style>{`path{fill:none;stroke:#fff;stroke-width:2px;}`}</style>
     </defs>
     <g transform="translate(0.707 22.107) rotate(-90)">
       <path d="M0,0H19.434" transform="translate(0 7.789)" />
       <path d="M0,15.752,7.876,7.876,0,0" transform="translate(12.816 0)" />
+    </g>
+  </svg>
+);
+
+export const icon_arrow_bottom = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="17.166"
+    height="51.413"
+    viewBox="0 0 17.166 51.413"
+  >
+    <defs>
+      <style>{`path{fill:none;stroke:#000;stroke-width:2px;}`}</style>
+    </defs>
+    <g transform="translate(16.459 29.307) rotate(90)">
+      <path d="M0,0H50" transform="translate(-29.307 7.963)" />
+      <path d="M0,0,7.876,7.876,0,15.752" transform="translate(12.816 0)" />
     </g>
   </svg>
 );
--- src/components/ScrollTopButton.jsx
+++ src/components/ScrollTopButton.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { icon_top } from '../assets/svg/Svg';
+import { icon_arrow_bottom, icon_arrow_top } from '../assets/svg/Svg';
 
 const ScrollTopButton = () => {
   return (
-    <button className="btn scroll-top-btn">
-      <i className="ico">{icon_top}</i>
+    <button className="btn btn-scroll-top">
+      <span className="circle top" />
+      <span className="box">
+        <i className="ico">{icon_arrow_bottom}</i>
+      </span>
+      <span className="circle bottom">
+        <i className="ico">{icon_arrow_top}</i>
+      </span>
     </button>
   );
 };
