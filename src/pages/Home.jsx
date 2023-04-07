61a870b9ba234b7b720cd346f3a883f87472ccb5
kimmmm126 <rlarkgml88@gmail.com>
2023-04-07 18:39:31 +0900

    main section 동영상 bg 파일 추가 및 적용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import ScrollTopButton from '../components/ScrollTopButton';
+import VideoBg from '../assets/video/index.mp4';
 
 const Home = () => {
-  return <h1>home페이지</h1>;
+  return (
+    <>
+      <main>
+        <section className="sec-m">
+          <div className="video-bg">
+            <video src={VideoBg} autoPlay muted loop width="100%" />
+          </div>
+          <div className="container">
+            <div className="txt-area">
+              누구나
+              <br />
+              마음 편히
+              <br />놀 수 있게
+            </div>
+          </div>
+        </section>
+        <section className="sec-01" />
+        <section className="sec-02" />
+      </main>
+      <ScrollTopButton />
+    </>
+  );
 };
+
 export default Home;
