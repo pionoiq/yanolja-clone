ebab7a3c295e4e7148845609c903382317d0e1f3
pionoiq <101159509+pionoiq@users.noreply.github.com>
2023-04-08 19:06:05 +0900

    footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from '../components/Button/Button';
+import Footer from '../components/Footer/Footer';
 
 const Home = () => {
   return (
     <>
       <h1>home페이지</h1>
       <Button>버튼이다</Button>
+      <Footer></Footer>
     </>
   );
 };
