03f93c54322c26ac612480a51993b319595635f9
kimmmm126 <rlarkgml88@gmail.com>
2023-04-09 02:52:48 +0900

    상단이동버튼 기능 추가

--- src/assets/svg/Svg.js
+++ src/assets/svg/Svg.js
@@ -3,11 +3,11 @@ import React from 'react';
 export const icon_arrow_top = (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.166 22.107">
     <defs>
-      <style>{`path{fill:none;stroke:#fff;stroke-width:2px;}`}</style>
+      <style>{`.a{fill:none;stroke:#000;stroke-width:2px;}`}</style>
     </defs>
     <g transform="translate(0.707 22.107) rotate(-90)">
-      <path d="M0,0H19.434" transform="translate(0 7.789)" />
-      <path d="M0,15.752,7.876,7.876,0,0" transform="translate(12.816 0)" />
+      <path className="a" d="M0,0H19.434" transform="translate(0 7.789)" />
+      <path className="a" d="M0,15.752,7.876,7.876,0,0" transform="translate(12.816 0)" />
     </g>
   </svg>
 );
@@ -20,7 +20,7 @@ export const icon_arrow_bottom = (
     viewBox="0 0 17.166 51.413"
   >
     <defs>
-      <style>{`path{fill:none;stroke:#000;stroke-width:2px;}`}</style>
+      <style>{`path{fill:none;stroke:#fff;stroke-width:2px;}`}</style>
     </defs>
     <g transform="translate(16.459 29.307) rotate(90)">
       <path d="M0,0H50" transform="translate(-29.307 7.963)" />
--- src/components/ScrollTopButton.jsx
+++ src/components/ScrollTopButton.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { icon_arrow_bottom, icon_arrow_top } from '../assets/svg/Svg';
 
 const ScrollTopButton = () => {
+  const [position, setPosition] = useState(0);
+  function onScroll() {
+    setPosition(window.scrollY);
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
+  const onScrollTop = () => {
+    let between = 7; // 이동 간격 시간
+    let scroll = window.setInterval(function () {
+      let pos = window.pageYOffset;
+      let step = 20; // 이동 크기 픽셀
+      if (pos > 0) {
+        window.scrollTo(0, pos - step);
+      } else {
+        window.clearInterval(scroll);
+      }
+    }, between);
+  };
+
   return (
-    <button className="btn btn-scroll-top">
+    <button className={`${position > 0 ? 'active' : ''} btn-scroll-top`} onClick={onScrollTop}>
       <span className="circle top" />
       <span className="box">
         <i className="ico">{icon_arrow_bottom}</i>
